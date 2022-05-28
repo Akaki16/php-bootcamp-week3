6fea5b7c6184c663cc52675a5b053e57c25c69ea
Akaki Burjanadze <akakiburjanadze17@gmail.com>
2022-05-28 05:10:17 -0700

    #2 Implement utility function which will be called to get api data

---
+++ COUNTRY-SEARCH/utils/services.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+    @param string $url
+    @param string $user_agent
+    @param array $http_header
+    @return array
+*/
+function get_api_data(string $url, string $user_agent, array $http_header): array  {
+    $ch = curl_init();
+
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_USERAGENT, $user_agent);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $http_header);
+
+    $result = curl_exec($ch);
+
+    if ($result === false) {
+        echo 'Error' . ' ' . curl_error($ch);
+    } else {
+        return json_decode($result, true);
+    }
+}
