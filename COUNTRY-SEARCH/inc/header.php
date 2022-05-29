022c45e5d3d2d015d4ed6f76a7718cef25f1ea1d
Akaki Burjanadze <akakiburjanadze17@gmail.com>
2022-05-29 06:05:04 -0700

    Update navbar links

--- COUNTRY-SEARCH/inc/header.php
+++ COUNTRY-SEARCH/inc/header.php
@@ -17,13 +17,13 @@
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
-<body>
+<body class="bg-slate-800">
 
     <nav class="bg-white shadow dark:bg-gray-800">
         <div class="container px-6 py-4 mx-auto md:flex md:justify-around md:items-center">
             <div class="flex items-center justify-between">
                 <div>
-                    <a class="text-2xl font-bold text-gray-800 transition-colors duration-200 transform dark:text-white lg:text-3xl hover:text-gray-700 dark:hover:text-gray-300" href="#">Country Search</a>
+                    <a class="text-2xl font-bold text-gray-800 transition-colors duration-200 transform dark:text-white lg:text-3xl hover:text-gray-700 dark:hover:text-gray-300" href="index.php">Country Search</a>
                 </div>
 
                 <!-- Mobile menu button -->
@@ -39,8 +39,8 @@
             <!-- Mobile Menu open: "block", Menu closed: "hidden" -->
             <div class="items-center md:flex">
                 <div class="flex flex-col md:flex-row md:mx-6">
-                    <a class="my-1 text-sm font-medium text-gray-700 transition-colors duration-200 transform dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 md:mx-4 md:my-0" href="#">Search</a>
-                    <a class="my-1 text-sm font-medium text-gray-700 transition-colors duration-200 transform dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 md:mx-4 md:my-0" href="#">All</a>
+                    <a class="my-1 text-sm font-medium text-gray-700 transition-colors duration-200 transform dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 md:mx-4 md:my-0" href="index.php">Search</a>
+                    <a class="my-1 text-sm font-medium text-gray-700 transition-colors duration-200 transform dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 md:mx-4 md:my-0" href="countries.php">All</a>
                 </div>
             </div>
         </div>
